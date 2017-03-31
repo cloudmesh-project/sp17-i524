bb920651984ffe46554c6c722d4cc0a28d813051
Sabyasachi Roy Choudhury <sabyasachi087@gmail.com>
2017-03-31 20:25:50 +0530

    add:usr: Updated Weather data rest apis

--- project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/ncdc/weather_services.py
+++ project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/ncdc/weather_services.py
@@ -7,10 +7,29 @@ NCDC_SERVICES = AppUtil.enum(DATASETS='/datasets', DATA_CATEGORIES='/datacategor
                              LOCATION_CATEORIES='/locationcategories' , LOCATIONS='/locations', \
                              STATIONS='/stations', WEATHER_DATA='/data')
 
+DATA_TYPES = AppUtil.enum(PRECIPITATION='PRCP', SNOWFALL = 'SNOW', TEMP_AVG='TAVG' \
+                          , TEMP_MAX = 'TMAX', TEMP_MIN = 'TMIN'  )
+
 TOKEN = 'uBsxpIavTKiqKAMlyMrwkkvemOQQfpTg'
+HEADERS = {'token':TOKEN}
 
 def get_stations(country='FIPS:IN', offset=0):
     url = NCDC_API + NCDC_SERVICES.STATIONS + '?locationid=' + country + '&offset=' + str(offset)
-    headers = {'token':TOKEN}
-    response = requests.get(url, headers=headers)
+    
+    response = requests.get(url, headers=HEADERS)
     return response.json()
+
+def prepare_dataTypeUrl():
+    dataTypeId = '&datatypeid='
+    url = dataTypeId+DATA_TYPES.PRECIPITATION+dataTypeId \
+    + DATA_TYPES.SNOWFALL + dataTypeId + DATA_TYPES.TEMP_AVG + dataTypeId + DATA_TYPES.TEMP_MAX + dataTypeId \
+    + DATA_TYPES.TEMP_MIN 
+    return url
+
+def get_weather_data(startDate,endDate,stationId,country='FIPS:IN', offset=0):
+    url = NCDC_API+NCDC_SERVICES.WEATHER_DATA+'?datasetid=GSOM'+prepare_dataTypeUrl()+ \
+     '&stationid=' + stationId + '&startdate=' + startDate + '&enddate=' \
+     + endDate + '?locationid=' + country + '&offset=' + str(offset)
+    response = requests.get(url, headers=HEADERS) 
+    return response.json()
+
