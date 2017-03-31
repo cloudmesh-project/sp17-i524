49a83b50dab20d9ad7081d223f641ba673c0ab56
Sabyasachi Roy Choudhury <sabyasachi087@gmail.com>
2017-03-31 20:25:50 +0530

    add:usr: Updated Weather data rest apis

--- project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/data/data_service.py
+++ project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/data/data_service.py
@@ -1,5 +1,8 @@
 import iu.i524.S17IRP013.ncdc.weather_services as Services
 
+
+
+
 def load_stations():
     limit = 25    
     offset = 0
@@ -8,4 +11,12 @@ def load_stations():
         print result['results']
         offset = offset + limit
         result = Services.get_stations(offset=offset)
-    
+
+def load_weather_data():
+    limit = 25    
+    offset = 0
+    result = Services.get_weather_data(startDate='1968-01-01',endDate='1970-01-01',stationId='GHCND:IN001011001')   
+    while(result!={}):        
+        print result['results']
+        offset = offset + limit
+        result = Services.get_stations(offset=offset)
