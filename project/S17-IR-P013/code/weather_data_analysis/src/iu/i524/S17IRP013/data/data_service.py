41c45336099a36787c6b178bb2eef2e779786c15
Sabyasachi Roy Choudhury <sabyasachi087@gmail.com>
2017-04-05 07:07:40 +0530

    usr:modified: This is to connect Hbase thrift server and dump all station data into it

--- project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/data/data_service.py
+++ project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/data/data_service.py
@@ -1,17 +1,28 @@
 import iu.i524.S17IRP013.ncdc.weather_services as Services
+import iu.i524.S17IRP013.dao.stations_dao as stations
 
 
-
-
+###############################################################
 def load_stations():
     limit = 25    
     offset = 0
+    nbr_of_records = 0
     result = Services.get_stations(offset=offset)   
-    while(result!={}):        
-        print result['results']
+    while(result!={}):       
+        nbr_of_records = nbr_of_records + insert_station(result['results'])
         offset = offset + limit
         result = Services.get_stations(offset=offset)
-
+    print str(nbr_of_records) + ' stations loaded successfully !!'    
+###############################################################
+def insert_station(results):
+    count = 0 
+    for data in results:
+        cell = stations.get_cell(data)
+        row_key = str(data['id'])
+        stations.insert(row_key, cell)
+        count = count + 1
+    return count    
+###############################################################
 def load_weather_data():
     limit = 25    
     offset = 0
@@ -20,3 +31,4 @@ def load_weather_data():
         print result['results']
         offset = offset + limit
         result = Services.get_stations(offset=offset)
+###############################################################
--- project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/test/TestRunner.py
+++ project/S17-IR-P013/code/weather_data_analysis/src/iu/i524/S17IRP013/test/TestRunner.py
@@ -1,3 +1,5 @@
 import iu.i524.S17IRP013.data.data_service as DataService
 
-DataService.load_weather_data()
+
+DataService.load_stations();
+#DataService.load_weather_data()
