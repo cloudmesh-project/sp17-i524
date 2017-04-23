da20ac5df3e8865139e0488cd6d2584e186c87ef
Scott McClary <scmcclar@indiana.edu>
2017-04-23 18:17:26 -0400

    new:usr: adding ansible env. variables

--- project/S17-IO-3011/code/scripts/deploy.sh
+++ project/S17-IO-3011/code/scripts/deploy.sh
@@ -4,18 +4,13 @@ cd ansible
 
 #Parse input (if exists) otherwise default to 3 nodes
 if [ "$#" -eq 0 ]; then
-    num_nodes=3
+    export num_nodes=3
 else 
-    num_nodes=$1
+    export num_nodes=$1
 fi
-
-#define some variables
-image=CC-Ubuntu14.04
-flavor=m1.medium
-cloud=chameleon
 
 #Turn off SSH authenticity checking
 export ANSIBLE_HOST_KEY_CHECKING=false
 
 #Run ansible playbook:
-ansible-playbook deploy.yml -i inventory.txt --extra-vars "num_nodes=$num_nodes image=$image flavor=$flavor cloud=$cloud"
+ansible-playbook deploy.yml -i inventory.txt --extra-vars "num_nodes=$num_nodes"
