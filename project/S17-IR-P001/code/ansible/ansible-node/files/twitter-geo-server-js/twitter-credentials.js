8accba5d807efe01af4c2983c62d657d98181aa1
sriramsitharaman <sriramsitharaman@gmail.com>
2017-05-17 02:15:10 +0530

    chg: usr: updated the ansible code

---
+++ project/S17-IR-P001/code/ansible/ansible-node/files/twitter-geo-server-js/twitter-credentials.js
@@ -0,0 +1,10 @@
+/*
+ * You need to supply your own values for the following keys.
+ * Get yours at http://apps.twitter.com
+ */
+module.exports = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+};
