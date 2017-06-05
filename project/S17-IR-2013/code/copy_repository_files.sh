235b659fc155765bbe511eabce8ac4b3a844d7f9
Sahiti Korrapati <sahitikorrapati@gmail.com>
2017-06-05 18:31:28 -0400

    chg:usr reduced the number of steps

--- project/S17-IR-2013/code/copy_repository_files.sh
+++ project/S17-IR-2013/code/copy_repository_files.sh
@@ -1,7 +1,11 @@
 bak_var=`date +%H%M%S`
-if [ -d "$HOME/outbrain" ]; then
- mv $HOME/outbrain $HOME/$bak_var
+outbrain=$HOME/outbrain
+if [ -d "$outbrain" ]; then
+ mv $outbrain $HOME/$bak_var
 fi;
-mkdir $HOME/outbrain
-find ~/classes/github/cloudmesh/sp17-i524/project/S17-IR-2013/code -type f -print0 | xargs -0 cp -t ~/outbrain
+mkdir $outbrain
+find ~/classes/github/cloudmesh/sp17-i524/project/S17-IR-2013/code -type f -print0 | xargs -0 cp -t $outbrain
 ##mv --backup=t <source_file> <dest_file>
+cd $HOME/outbrain
+wget https://iu.box.com/s/3mb9aiegh6boswlbbe3yg14if8fcfkfq 
+mv $outbrain/3mb9aiegh6boswlbbe3yg14if8fcfkfq $outbrain/dataset.tgz
