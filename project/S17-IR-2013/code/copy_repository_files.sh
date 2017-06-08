ef7c31ac8e032c921f9fab06444f83ea6cc9c0b6
Sahiti Korrapati <sahitikorrapati@gmail.com>
2017-06-07 21:07:44 -0400

    cng:usr: Making minor changes to report

--- project/S17-IR-2013/code/copy_repository_files.sh
+++ project/S17-IR-2013/code/copy_repository_files.sh
@@ -7,5 +7,5 @@ mkdir $outbrain
 find ~/classes/github/cloudmesh/sp17-i524/project/S17-IR-2013/code -type f -print0 | xargs -0 cp -t $outbrain
 ##mv --backup=t <source_file> <dest_file>
 cd $HOME/outbrain
-wget https://iu.box.com/s/3mb9aiegh6boswlbbe3yg14if8fcfkfq 
-mv $outbrain/3mb9aiegh6boswlbbe3yg14if8fcfkfq $outbrain/dataset.tgz
+wget https://iu.box.com/shared/static/3mb9aiegh6boswlbbe3yg14if8fcfkfq.tgz 
+mv $outbrain/3mb9aiegh6boswlbbe3yg14if8fcfkfq.tgz $outbrain/dataset.tgz
